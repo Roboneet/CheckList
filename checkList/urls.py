e37878640a96b94f6a8fa09e28dd534e926356a9
Roboneet <neethumariyajoy@gmail.com>
2017-05-22 00:55:08 +0530

    index view and details view

--- checkList/urls.py
+++ checkList/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$',views.index,name="index"),
+	url(r'^item/(?P<pk>\d+)$',views.item_detail, name="item_detail"),
 ]
--- checkList/views.py
+++ checkList/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Item
 # Create your views here.
 
 def index(request):
 	items = Item.objects.all()
 	return render(request,'checkList/main.html',{'items':items})
+
+
+def item_detail(request,pk):
+	item = get_object_or_404(Item,pk=pk)
+	return render(request,'checklist/item_details.html',{'item': item})
